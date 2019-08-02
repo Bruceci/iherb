2a277880c24a33915d9253d784020b238587035d
bruceci <18217016834@163.com>
2019-08-03 03:50:54 +0800

    changed

--- page.js
+++ page.js
@@ -1,12 +1,11 @@
 const puppeteer = require("puppeteer-extra");
 const pluginStealth = require("puppeteer-extra-plugin-stealth");
 puppeteer.use(pluginStealth());
-// const chrome_driver = process.env["CHROME_DRIVER"];
+const chrome_driver = process.env["CHROME_DRIVER"];
 const args = [
   "--no-sandbox",
   "--disable-setuid-sandbox",
   "--disable-infobars",
-  "--window-position=0,0",
   "--ignore-certificate-errors",
   "--ignore-certificate-errors-spki-list",
   "--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
@@ -16,7 +15,7 @@ module.exports = puppeteer
   .launch({
     ignoreHTTPSErrors: true,
     userDataDir: "./tmp1",
-    // executablePath: chrome_driver,
+    executablePath: chrome_driver,
     headless: true,
     defaultViewport: null,
     args: args
